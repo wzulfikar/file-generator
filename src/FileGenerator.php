a57a4ec347a098a2014b5773bcb63365112240ec
= <wzulfikar@ymail.com>
2015-12-14 17:29:51 +0800

    rename exect to render

--- src/FileGenerator.php
+++ src/FileGenerator.php
@@ -2,35 +2,40 @@
 
 namespace Wzulfikar\FileGenerator;
 
+use Philo\Blade\Blade;
+
 class FileGenerator{
 	
 	public $vars;
-	public $template;
+	public $view;
+	public $renderer;
+	
+	public $views = __DIR__ . '/../views';
+	public $cache = __DIR__ . '/../cache';
 
-	public function __construct($template, array $vars = [])
+	/**
+	 * [__construct description]
+	 * @param string $template path to template file
+	 * @param array  $vars     array of variables to inject
+	 */
+	public function __construct($view, array $vars = [])
 	{
-		if ( ! is_string($template) ) 
-			throw new Exception('Type of $template should be string instead of ' . gettype($template));
-		$this->template = $template;
+		$this->views = dirname( $view );
+		$this->view  = basename( $view );
 		$this->setVars($vars);
+		$this->renderer = new Blade($this->views, $this->cache);
 	}
 	
 	public function setVars(array $vars){
 		$this->vars = $vars;
 	}
 	
-	public function exec(){
-		return $this->replace_template_vars($this->template, $this->vars);
+	public function render($view = null){
+		$view = $view ?: $this->view;
+		return $this->renderer->view()->make($view, $this->vars)->render();
 	}
 	
 	public function put($output){
-		file_put_contents($output, $this->exec());
-	}
-
-	public function replace_template_vars($template, array $vars){
-	    foreach ($vars as $var_name_to_replace => $new_var_name) {
-	        $template = str_replace('{{' . $var_name_to_replace . '}}', $new_var_name, $template);
-	    }
-	    return $template;
+		file_put_contents($output, $this->render());
 	}
 }
